26a1796f83eff41a8f3bf8d6dd582f0c97ad4229
yamen <hzmkiller@gmail.com>
2023-04-28 15:22:18 +0300

    new changes

--- frontend/project/src/Compoents/SideBar/SideBar.js
+++ frontend/project/src/Compoents/SideBar/SideBar.js
@@ -3,16 +3,20 @@ import { Link } from "react-router-dom"
 import './sidebar.css'
 import { useContext } from "react"
 import { ThemeContext } from "../../ThemeContext"
+import logo from '../../imgs/logo.png'
+import userlogo from '../../imgs/profilestu.jpg'
 
 
 function SlideBar() {
     const Theme = useContext(ThemeContext).Theme
     const [sidebarstate, settogglesidebar] = useState(true)
+    let [sidebarArrow, setArrow] = useState("<")
     const sidebar = useRef('')
 
 
 
     function togglesidebar() {
+
         settogglesidebar(prevvalue => !prevvalue)
 
         if (sidebarstate) {
@@ -22,6 +26,11 @@ function SlideBar() {
                 text.style.display = "none"
 
             })
+            let arrow = document.querySelector(".SideBar-Students .sidebar-arrow")
+            arrow.classList.add("active")
+            let logotext = document.querySelector(".SideBar-Students .sidebar-logo p")
+            logotext.style.visibility = "hidden"
+
         }
         else {
             sidebar.current.style.width = "220px"
@@ -29,6 +38,10 @@ function SlideBar() {
             sidebaritemtext.forEach(text => {
                 text.style.display = "inline"
             })
+            let arrow = document.querySelector(".SideBar-Students .sidebar-arrow")
+            arrow.classList.remove("active")
+            let logotext = document.querySelector(".SideBar-Students .sidebar-logo p")
+            logotext.style.visibility = "visible"
         }
     }
 
@@ -49,18 +62,20 @@ function SlideBar() {
     }
     return (
         <div className="SideBar-Students" ref={sidebar} style={{ backgroundColor: Theme.sideBar }}>
-            <div>
-                <h2>hello wrold</h2>
-            </div>
-            <ul className="Links list-unstyled position-sticky">
-                <div>
-                    <button onClick={togglesidebar} className=" position-sticky  border-3 border-white bg-transparent text-white">|||</button>
-                    
+            <ul className="Links list-unstyled position-sticky bg-danger">
+                <div className="sidebar-logo d-flex align-items-center gap-1 flex-wrap pt-3">
+                    <img src={logo} alt="logoimg" width={40} />
+                    <p>SAWA</p>
+                </div>
+                <div className=" d-flex justify-content-end">
+                    <button onClick={togglesidebar} className="sidebar-arrow px-3 fs-4 position-sticky border-0  bg-transparent text-center text-white">
+                        &lt;
+                    </button>
                 </div>
                 <li className="active">
                     <Link onClick={sideBarliToggle} className="link d-flex" to={'/stu'}>
                         <span className="me-2 icon">
-                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fillRule={Theme.sideBar} className="bi bi-book" viewBox="0 0 16 16">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="17" height="17" fillRule={Theme.sideBar} className="bi bi-book" viewBox="0 0 16 16">
                                 <path d="M1 2.828c.885-.37 2.154-.769 3.388-.893 1.33-.134 2.458.063 3.112.752v9.746c-.935-.53-2.12-.603-3.213-.493-1.18.12-2.37.461-3.287.811V2.828zm7.5-.141c.654-.689 1.782-.886 3.112-.752 1.234.124 2.503.523 3.388.893v9.923c-.918-.35-2.107-.692-3.287-.81-1.094-.111-2.278-.039-3.213.492V2.687zM8 1.783C7.015.936 5.587.81 4.287.94c-1.514.153-3.042.672-3.994 1.105A.5.5 0 0 0 0 2.5v11a.5.5 0 0 0 .707.455c.882-.4 2.303-.881 3.68-1.02 1.409-.142 2.59.087 3.223.877a.5.5 0 0 0 .78 0c.633-.79 1.814-1.019 3.222-.877 1.378.139 2.8.62 3.681 1.02A.5.5 0 0 0 16 13.5v-11a.5.5 0 0 0-.293-.455c-.952-.433-2.48-.952-3.994-1.105C10.413.809 8.985.936 8 1.783z" />
                             </svg>
                         </span>
@@ -79,7 +94,7 @@ function SlideBar() {
 
 
                 <li><Link onClick={sideBarliToggle} className="link  d-flex" to={'/stu/allcourses'}>
-                    <span className="me-2"> <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fillRule="currentColor" className="bi bi-card-list" viewBox="0 0 16 16">
+                    <span className="me-2"> <svg xmlns="http://www.w3.org/2000/svg" width="17" height="17" fillRule="currentColor" className="bi bi-card-list" viewBox="0 0 16 16">
                         <path d="M14.5 3a.5.5 0 0 1 .5.5v9a.5.5 0 0 1-.5.5h-13a.5.5 0 0 1-.5-.5v-9a.5.5 0 0 1 .5-.5h13zm-13-1A1.5 1.5 0 0 0 0 3.5v9A1.5 1.5 0 0 0 1.5 14h13a1.5 1.5 0 0 0 1.5-1.5v-9A1.5 1.5 0 0 0 14.5 2h-13z" />
                         <path d="M5 8a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7A.5.5 0 0 1 5 8zm0-2.5a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7a.5.5 0 0 1-.5-.5zm0 5a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7a.5.5 0 0 1-.5-.5zm-1-5a.5.5 0 1 1-1 0 .5.5 0 0 1 1 0zM4 8a.5.5 0 1 1-1 0 .5.5 0 0 1 1 0zm0 2.5a.5.5 0 1 1-1 0 .5.5 0 0 1 1 0z" />
                     </svg></span><span className="sidebar-itme-text">Courses</span></Link></li>
@@ -87,42 +102,44 @@ function SlideBar() {
 
                 <li><Link onClick={sideBarliToggle} className="link d-flex" to={'/stu/news'}>
                     <span className="me-2">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fillRule="currenColor" className="bi bi-chat-dots" viewBox="0 0 16 16">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="17" height="17" fillRule="currenColor" className="bi bi-chat-dots" viewBox="0 0 16 16">
                             <path d="M5 8a1 1 0 1 1-2 0 1 1 0 0 1 2 0zm4 0a1 1 0 1 1-2 0 1 1 0 0 1 2 0zm3 1a1 1 0 1 0 0-2 1 1 0 0 0 0 2z" />
                             <path d="m2.165 15.803.02-.004c1.83-.363 2.948-.842 3.468-1.105A9.06 9.06 0 0 0 8 15c4.418 0 8-3.134 8-7s-3.582-7-8-7-8 3.134-8 7c0 1.76.743 3.37 1.97 4.6a10.437 10.437 0 0 1-.524 2.318l-.003.011a10.722 10.722 0 0 1-.244.637c-.079.186.074.394.273.362a21.673 21.673 0 0 0 .693-.125zm.8-3.108a1 1 0 0 0-.287-.801C1.618 10.83 1 9.468 1 8c0-3.192 3.004-6 7-6s7 2.808 7 6c0 3.193-3.004 6-7 6a8.06 8.06 0 0 1-2.088-.272 1 1 0 0 0-.711.074c-.387.196-1.24.57-2.634.893a10.97 10.97 0 0 0 .398-2z" />
                         </svg></span><span className="sidebar-itme-text">News</span></Link></li>
 
 
                 <li><Link onClick={sideBarliToggle} className="link d-flex" to={'/stu/library'}>
-                    <span className="me-2"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fillRule="currentColor" className="bi bi-person-lines-fill-rule" viewBox="0 0 16 16">
+                    <span className="me-2"><svg xmlns="http://www.w3.org/2000/svg" width="17" height="17" fillRule="currentColor" className="bi bi-person-lines-fill-rule" viewBox="0 0 16 16">
                         <path d="M6 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm-5 6s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H1zM11 3.5a.5.5 0 0 1 .5-.5h4a.5.5 0 0 1 0 1h-4a.5.5 0 0 1-.5-.5zm.5 2.5a.5.5 0 0 0 0 1h4a.5.5 0 0 0 0-1h-4zm2 3a.5.5 0 0 0 0 1h2a.5.5 0 0 0 0-1h-2zm0 3a.5.5 0 0 0 0 1h2a.5.5 0 0 0 0-1h-2z" />
                     </svg></span><span className="sidebar-itme-text">Library</span></Link></li>
 
 
                 <li><Link onClick={sideBarliToggle} className="link d-flex" to={'/stu/chat'}>
-                    <span className="me-2"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fillRule="currentColor" className="bi bi-chat-dots" viewBox="0 0 16 16">
+                    <span className="me-2"><svg xmlns="http://www.w3.org/2000/svg" width="17" height="17" fillRule="currentColor" className="bi bi-chat-dots" viewBox="0 0 16 16">
                         <path d="M5 8a1 1 0 1 1-2 0 1 1 0 0 1 2 0zm4 0a1 1 0 1 1-2 0 1 1 0 0 1 2 0zm3 1a1 1 0 1 0 0-2 1 1 0 0 0 0 2z" />
                         <path d="m2.165 15.803.02-.004c1.83-.363 2.948-.842 3.468-1.105A9.06 9.06 0 0 0 8 15c4.418 0 8-3.134 8-7s-3.582-7-8-7-8 3.134-8 7c0 1.76.743 3.37 1.97 4.6a10.437 10.437 0 0 1-.524 2.318l-.003.011a10.722 10.722 0 0 1-.244.637c-.079.186.074.394.273.362a21.673 21.673 0 0 0 .693-.125zm.8-3.108a1 1 0 0 0-.287-.801C1.618 10.83 1 9.468 1 8c0-3.192 3.004-6 7-6s7 2.808 7 6c0 3.193-3.004 6-7 6a8.06 8.06 0 0 1-2.088-.272 1 1 0 0 0-.711.074c-.387.196-1.24.57-2.634.893a10.97 10.97 0 0 0 .398-2z" />
                     </svg></span><span className="sidebar-itme-text">Chat</span></Link></li>
 
 
                 <li><Link onClick={sideBarliToggle} className="link d-flex" to={'/stu/profile'}>
-                    <span className="me-2"><svg xmlns="http://wwwprofile.w3.org/2000/svg" width="16" height="16" fillRule="currentColor" className="bi bi-person-badge-fill-rule" viewBox="0 0 16 16">
+                    <span className="me-2"><svg xmlns="http://wwwprofile.w3.org/2000/svg" width="17" height="17" fillRule="currentColor" className="bi bi-person-badge-fill-rule" viewBox="0 0 16 16">
                         <path d="M2 2a2 2 0 0 1 2-2h8a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V2zm4.5 0a.5.5 0 0 0 0 1h3a.5.5 0 0 0 0-1h-3zM8 11a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm5 2.755C12.146 12.825 10.623 12 8 12s-4.146.826-5 1.755V14a1 1 0 0 0 1 1h8a1 1 0 0 0 1-1v-.245z" />
                     </svg></span><span className="sidebar-itme-text">Profile</span></Link></li>
 
                 <li><Link onClick={sideBarliToggle} className="link d-flex" to={'/stu/settings'}>
-                    <span className="me-2"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fillRule="currentColor" className="bi bi-gear-fill-rule" viewBox="0 0 16 16">
+                    <span className="me-2"><svg xmlns="http://www.w3.org/2000/svg" width="17" height="17" fillRule="currentColor" className="bi bi-gear-fill-rule" viewBox="0 0 16 16">
                         <path d="M9.405 1.05c-.413-1.4-2.397-1.4-2.81 0l-.1.34a1.464 1.464 0 0 1-2.105.872l-.31-.17c-1.283-.698-2.686.705-1.987 1.987l.169.311c.446.82.023 1.841-.872 2.105l-.34.1c-1.4.413-1.4 2.397 0 2.81l.34.1a1.464 1.464 0 0 1 .872 2.105l-.17.31c-.698 1.283.705 2.686 1.987 1.987l.311-.169a1.464 1.464 0 0 1 2.105.872l.1.34c.413 1.4 2.397 1.4 2.81 0l.1-.34a1.464 1.464 0 0 1 2.105-.872l.31.17c1.283.698 2.686-.705 1.987-1.987l-.169-.311a1.464 1.464 0 0 1 .872-2.105l.34-.1c1.4-.413 1.4-2.397 0-2.81l-.34-.1a1.464 1.464 0 0 1-.872-2.105l.17-.31c.698-1.283-.705-2.686-1.987-1.987l-.311.169a1.464 1.464 0 0 1-2.105-.872l-.1-.34zM8 10.93a2.929 2.929 0 1 1 0-5.86 2.929 2.929 0 0 1 0 5.858z" />
                     </svg></span><span className="sidebar-itme-text">Settings</span></Link></li>
 
-                <li><Link onClick={sideBarliToggle} className="link d-flex" to={'/'}>
-                    <span className="me-2"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fillRule="currentColor" className="bi bi-box-arrow-in-left" viewBox="0 0 16 16">
-                        <path fillRule="evenodd" d="M10 3.5a.5.5 0 0 0-.5-.5h-8a.5.5 0 0 0-.5.5v9a.5.5 0 0 0 .5.5h8a.5.5 0 0 0 .5-.5v-2a.5.5 0 0 1 1 0v2A1.5 1.5 0 0 1 9.5 14h-8A1.5 1.5 0 0 1 0 12.5v-9A1.5 1.5 0 0 1 1.5 2h8A1.5 1.5 0 0 1 11 3.5v2a.5.5 0 0 1-1 0v-2z" />
-                        <path fillRule="evenodd" d="M4.146 8.354a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L5.707 7.5H14.5a.5.5 0 0 1 0 1H5.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3z" />
-                    </svg></span><span className="sidebar-itme-text">Log Out</span></Link></li>
-
             </ul>
+                <div className="d-flex bg-black position-fixed p-3 bottom-0 w-100">
+                    <img src={userlogo} width={30} />
+                    <Link onClick={sideBarliToggle} className="link d-flex flex-wrap" to={'/'}>
+                        <span className="me-2"><svg xmlns="http://www.w3.org/2000/svg" width="17" height="17" fillRule="currentColor" className="bi bi-box-arrow-in-left" viewBox="0 0 16 16">
+                            <path fillRule="evenodd" d="M10 3.5a.5.5 0 0 0-.5-.5h-8a.5.5 0 0 0-.5.5v9a.5.5 0 0 0 .5.5h8a.5.5 0 0 0 .5-.5v-2a.5.5 0 0 1 1 0v2A1.5 1.5 0 0 1 9.5 14h-8A1.5 1.5 0 0 1 0 12.5v-9A1.5 1.5 0 0 1 1.5 2h8A1.5 1.5 0 0 1 11 3.5v2a.5.5 0 0 1-1 0v-2z" />
+                            <path fillRule="evenodd" d="M4.146 8.354a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L5.707 7.5H14.5a.5.5 0 0 1 0 1H5.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3z" />
+                        </svg></span><span className="sidebar-itme-text">Log Out</span></Link>
+                </div>
         </div>
     )
 }
